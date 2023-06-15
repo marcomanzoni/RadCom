950b4893082b09a21d982d779d4d614a4c050539
Marco Manzoni <marcomanzoni@users.noreply.github.com>
2023-06-15 12:30:40 +0200

    Fixed delay

--- radcom.m
+++ radcom.m
@@ -5,6 +5,9 @@ clear all;
 close all;
 clc;
 
+load('./lajolla.mat');
+lajolla = flip(lajolla);
+
 %% Antenna system parameter
 f0                  = 4e9;              % Carrier frequency [Hz]
 c                   = physconst("lightspeed");
@@ -15,17 +18,17 @@ Nx_tx               = 1;                % Number of antennae in the horizontal p
 Nz_tx               = 1;                % Number of antennae in the vertical plane
 dx                  = lambda/4;         % Antenna spacing in the horizontal plane
 dz                  = dx;               % Antenna spacing in the vertical plane
-installation_height = 20;               % The height of the antenna over the ground plane
+installation_height = 50;               % The height of the antenna over the ground plane
 delta_psi           = 100/180*pi;       % azimuth beamwidth [rad]
 delta_teta          = 100/180*pi;       % elevation beamwidth [rad]
-teta_point          = 0/180*pi;         % antenna pointing in elevation [rad] 
+teta_point          = 0/180*pi;         % antenna pointing in elevation [rad]
 psi_point           = 0/180*pi;         % antenna pointing in azimuth [rad]
 
-Ptx_db              = -30;              % Total transmission power [dBw] (dBw = dBm - 30)
+Ptx_db              = -7;              % Total transmission power [dBw] (dBw = dBm - 30)
 eff_ant             = 0.7;              % Antenna efficiency
 T0_ant              = 290;              % Antenna temperature [K]
 T_scene             = 290;              % Scene temperature [K]
-F_dB                = 10;                % Receiver noise figure [dB]
+F_dB                = 13;               % Receiver noise figure [dB]
 T0                  = 290;              % Temperature at which the noise figure is referring to [K]
 
 %% Parameters for the 5G sub-6 system
@@ -36,14 +39,14 @@ u                 = 0;                  % Numerology
 delta_f           = 15e3 * 2^u;         % Sub-carrier spacing (defined in this way by the standard)
 Ts_no_cp          = 1/delta_f;          % Length of the OFDM symbol [s]
 
-cpPercentage      = 7;                  % Percentage of the symbol dedicated to cyclic prefix
+cpPercentage      = 0;                  % Percentage of the symbol dedicated to cyclic prefix
 Ts_tot            = Ts_no_cp*(1+cpPercentage/100); % Total symbol time, including cyclic prefix
 
 subCarrierPerRB   = 12;                 % Number of sub-carrier for each resource block;
 numberOfRB        = ceil(B/(subCarrierPerRB*delta_f)); % Total number of RB in frequency;
 totalSubcarriers  = ceil(B/delta_f);    % Total number of sub-carriers
 frameDuration     = 10e-3;              % Duration of the frame [s];
-Nsymbols          = floor(frameDuration/Ts_tot); 
+Nsymbols          = floor(frameDuration/Ts_tot);
 
 subframePerFrame  = 10;                 % Number of sub-frame in the frame
 subFrameDuration  = frameDuration/subframePerFrame; % Duration of each sub-frame [s]
@@ -67,14 +70,14 @@ title("OFDM Frame"); axis xy tight
 qpsk_modulated_data = pskmod(frame, M);
 
 % Simulate continuous time signal
-fs                  = 2*B;     % Sampling frequency in fast time [Hz];
+fs                  = 4*B;     % Sampling frequency in fast time [Hz];
 dt                  = 1/fs;   % sampling in fast time [s]
 t                   = -Ts_tot:dt:Ts_tot;% Fast time axis
 r                   = t*c/2; % Range axis
 rho_rg              = c/(2*B); % Slant range resolution c/2B
 
-% Define the frequency domain axis
-NFFT                = 2048;
+% Define the frequency domain axis to check the bandwidth occupancy
+NFFT                = 2^nextpow2(length(t));
 df                  = fs/NFFT;
 f_ax                = (-NFFT/2:NFFT/2-1)*df;
 
@@ -82,26 +85,25 @@ f_ax                = (-NFFT/2:NFFT/2-1)*df;
 % sub-carrier, in the second simension I sweep the symbols (same
 % sub-carrier) and along the third dimension I sweep all the sub-carrier.
 % If I sum along the third dimension I get for every colum the OFDM symbol.
-signal_tx = zeros(length(t), Nsymbols, totalSubcarriers);
-
-for ii = 1:totalSubcarriers
-    signal_tx(:,:,ii) = qpsk_modulated_data(ii,:).*exp(1j.*2*pi*(ii-1)*delta_f*t').*rectpuls(t'/Ts_tot);
-end
+subCarrierIndex = (0:totalSubcarriers-1); subCarrierIndex = floor(subCarrierIndex - mean(subCarrierIndex));
+modulationMatrix = exp(1j.*2*pi*t'*subCarrierIndex*delta_f);
+signal_tx_bb = modulationMatrix*qpsk_modulated_data.*rectpuls(t'/Ts_tot);
 
 % Visualize the spectrum of all the sub-carriers;
-figure; plot(f_ax, db(fftshift(fft(squeeze(signal_tx(:,1,:)),NFFT)))); grid on; hold on;
-xlabel("Frequency [Hz]"); title("Subcarriers of first OFDM symbol");
+figure; plot(f_ax, db(fftshift(fft(squeeze(signal_tx_bb(:,1:5)),NFFT)))); grid on; hold on;
+xlabel("Frequency [Hz]"); title("First 5 OFDM symbols");
 
-% Visualize the sum
-plot(f_ax, db(fftshift(fft(sum(signal_tx(:,1,:),3),NFFT))), 'r', LineWidth=2); 
+y = autocorrelation(signal_tx_bb(:,1), signal_tx_bb(:,1), dt);
+figure; plot(r,db(y)); grid on; xlim([-100,100]); ylabel("Amplitude [dB]"); xlabel("Range [m]");
+title("Autocorrelation of the first transmitted symbol");
 
-% Sum all the sub-carriers and bring it to passband for transmission
 % The recived signal will be the delayed and scaled version of this signal
-signal_tx = sum(signal_tx, 3).*exp(1j*2*pi*f0*t(:));
+signal_tx = signal_tx_bb;%.*exp(1j*2*pi*f0*t(:));
 
 %% Trajectory of the platform
 
 Sx = (0:Nsymbols-1)*lambda/4;
+Sx = Sx - mean(Sx);
 Sy = zeros(size(Sx));
 Sz = zeros(size(Sx));
 
@@ -110,16 +112,17 @@ RCS                 = 1; % Radar cross section [m^2]
 
 % Each line is a target, the colums are the x,y,z position. The center of
 % the reference system is the center of the base station
-p_t                 = [0, 100, -installation_height]; 
+p_t                 = [0, 100, -installation_height];
 
 %% Derive some parameters from numbers above
 
 Nt                  = size(p_t, 1);             % Number of targets
-
 beam_sector         = delta_teta*delta_psi;     % [rad^2]
 G                   = eff_ant*4*pi/beam_sector; % Antenna gain
 
-Ptx = 10^(Ptx_db/10);                           %[W]
+Ptx                 = 10^(Ptx_db/10);               %[W]
+EIRP_w              = Ptx*G;                        %[W]
+EIRP_dbm            = 10*log10(EIRP_w*1000)         % [dBm]
 
 % Antenna positions in reception. The transmission antenna is located at
 % the origin and transmits all the power
@@ -127,14 +130,14 @@ x_s = (0:Nx_tx-1)'*dx; x_s = x_s - mean(x_s); x_s = kron(ones(Nz_tx,1), x_s);
 z_s = (0:Nz_tx-1)'*dz; z_s = z_s - mean(z_s); z_s = kron(z_s, ones(Nx_tx,1));
 y_s = zeros(size(x_s));
 
-N_ant       = length(x_s);                      % Total number of antennas
-Ae          = (lambda^2)/4/pi*G;                % Equivalent area for each antenna
+N_ant       = length(x_s);                          % Total number of antennas
+Ae          = (lambda^2)/4/pi*G;                    % Equivalent area for each antenna
 
-F           = 10^(F_dB/10);                     % noise figure in linear unit (noise factor)
-Trx         = (F-1)*T0;                         % Receiver temperature [Kelvin]
+F           = 10^(F_dB/10);                         % noise figure in linear unit (noise factor)
+Trx         = (F-1)*T0;                             % Receiver temperature [Kelvin]
 T_ant       = eff_ant*T_scene + (1-eff_ant)*T0_ant; % Antenna temperature [Kelvin]
-T_sys       = T_ant + Trx;                      % System temperature [Kelvin]
-N0          = K*T_sys;                          % Noise Power Spectral Density [W/Hz]
+T_sys       = T_ant + Trx;                          % System temperature [Kelvin]
+N0          = K*T_sys;                              % Noise Power Spectral Density [W/Hz]
 
 %% Acquisition
 
@@ -143,9 +146,9 @@ Draw_n  = zeros(length(t), N_ant, Nsymbols);
 Drc     = zeros(length(t), N_ant, Nsymbols);
 Drc_n   = zeros(length(t), N_ant, Nsymbols);
 
-for kk = 1:Nsymbols
-    for ii = 1:N_ant
-        for n = 1:Nt
+for kk = 1:Nsymbols % symbols = slow time samples
+    for ii = 1:N_ant % Antennae
+        for n = 1:Nt % Targets
 
             % In transmission
             delta_x = Sx(kk)-p_t(n,1);
@@ -180,9 +183,15 @@ for kk = 1:Nsymbols
             Prx = Ss*Ae.*f; % f should be computed again for each receiver... approximation
 
             tau             = (R_tx+R_rx)/c; % Delay from distance (to change in case of bistatic)
-            delayFilter     = fs*sinc(fs*(t-tau)); % Filter that I use for delay
-            s               = sqrt(Prx)*conv(signal_tx(:,kk), delayFilter, 'same')*dt; % Delay the TX signal
-            s               = s.*exp(-1j*2*pi*f*t(:)); % Demodulate it
+
+            s = sqrt(Prx)*interp1(t', signal_tx(:,kk), t'-tau, "cubic", 0).*exp(-1j*2*pi*f0*tau);
+            %y = autocorrelation(s, signal_tx(:,kk), dt);
+            %figure; plot(r, abs(y));
+
+            %delayFilter     = fs*sinc(fs*(t-tau)); % Filter that I use for delay
+            %s               = sqrt(Prx)*conv(signal_tx(:,kk), delayFilter, 'same')*dt; % Delay the TX signal
+            %s               = s.*exp(-1j*2*pi*f*t(:)); % Demodulate it
+            
             Draw(:,ii,kk)   = Draw(:,ii,kk) + s; % Sum for every target
         end
 
@@ -211,29 +220,50 @@ x_foc = min(p_t(:,1))-30 : 0.1 : max(p_t(:,1))+30;
 y_foc = min(p_t(:,2))-30 : 0.1 : max(p_t(:,2))+30;
 
 [Y,X] = ndgrid(y_foc,x_foc);
-Z = zeros(size(X)); % I decide to focus on ground, but since I have 3D resolution I could focus other planes
+Z = -installation_height*ones(size(X)); % I decide to focus on ground, but since I have 3D resolution I could focus other planes
 
-I = zeros(size(X));
+I = zeros([size(X), Nsymbols]);
 I_n = zeros(size(I));
 
-for ii = 1:N_ant
-    delta_x = x_s(ii)-X;
-    delta_y = y_s(ii)-Y;
-    delta_z = z_s(ii)-Z;
+for kk = 1:Nsymbols % symbols = slow time samples
 
-    R     = sqrt(delta_x.^2+delta_y.^2+delta_z.^2);
-    tau   = 2*R/c; % Delay from distance (to change in case of bistatic)
+    fprintf("Focusing slow-time %d/%d: ", kk, Nsymbols);
 
-    I = I + interp1(t, Drc(:,ii), tau,"linear",NaN).*exp(+1j.*2*pi*f0*tau);
-    I_n = I_n + interp1(t, Drc_n(:,ii), tau,"linear",NaN).*exp(+1j.*2*pi*f0*tau);
+    delta_x = Sx(kk)-X;
+    delta_y = Sy(kk)-Y;
+    delta_z = Sz(kk)-Z;
+
+    R_tx = sqrt(delta_x.^2+delta_y.^2+delta_z.^2);
+
+    for ii = 1:N_ant
+        delta_x = (Sx(kk)+x_s(ii))-X;
+        delta_y = (Sy(kk)+y_s(ii))-Y;
+        delta_z = (Sz(kk)+z_s(ii))-Z;
+
+        R_rx = sqrt(delta_x.^2+delta_y.^2+delta_z.^2);
+
+        tau   = (R_tx + R_rx)/c; % Delay from distance (to change in case of bistatic)
+
+        I(:,:,kk) = I(:,:,kk) + interp1(t, Drc(:, ii, kk) , tau,"linear",NaN).*exp(+1j.*2*pi*f0*tau);
+        I_n(:,:,kk) = I_n(:,:,kk) + interp1(t, Drc_n(:,ii,kk), tau,"linear",NaN).*exp(+1j.*2*pi*f0*tau);
+    end
+    fprintf("Done. \n");
 end
 
-figure; imagesc(x_foc, y_foc, abs(I)); axis xy tight; title("Focused image without noise");
-xlabel("x [m]"); ylabel("y [m]");
+figure; imagesc(x_foc, y_foc, abs(I(:,:,1))); axis xy tight; title("MIMO Antenna without noise");
+xlabel("x [m]"); ylabel("y [m]"); colorbar; colormap(lajolla);
 
-figure; imagesc(x_foc, y_foc, abs(I_n)); axis xy tight; title("Focused image with noise");
-xlabel("x [m]"); ylabel("y [m]");
+figure; imagesc(x_foc, y_foc, abs(I_n(:,:,1))); axis xy tight; title("MIMO Antenna with noise");
+xlabel("x [m]"); ylabel("y [m]"); colorbar; colormap(lajolla);
 
+I_SAR = sum(I,3);
+I_SAR_n = sum(I_n,3);
+
+figure; imagesc(x_foc, y_foc, abs(I_SAR)); axis xy tight; title("SAR image without noise");
+xlabel("x [m]"); ylabel("y [m]"); colorbar; colormap(lajolla);
+
+figure; imagesc(x_foc, y_foc, abs(I_SAR_n)); axis xy tight; title("SAR image with noise");
+xlabel("x [m]"); ylabel("y [m]"); colorbar; colormap(lajolla);
 
 
 
